d38403a11ffbbc754cc02b654437515749e2cd3b
unknown <experte001@gmail.com>
2024-10-24 14:20:45 +0900

    API side Pagination:Pagination Booking has been done

--- client/src/features/bookings/useBookings.js
+++ client/src/features/bookings/useBookings.js
@@ -12,18 +12,22 @@ export function useBookings() {
       ? null
       : { field: "status", value: filterValue, method: "gte" };
 
+  //sort
   const sortByRaw = searchParams.get("sortBy") || "startDate-sesc";
   const [field, direction] = sortByRaw.split("-");
   const sortBy = { field, direction };
 
+  //pagination
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
+
   const {
     isLoading,
-    data: bookings,
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
-    queryKey: ["bookings", filter, sortBy],
-    queryFn: () => getBookings({ filter, sortBy }),
+    queryKey: ["bookings", filter, sortBy, page],
+    queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
-  return { isLoading, error, bookings };
+  return { isLoading, error, bookings, count };
 }
